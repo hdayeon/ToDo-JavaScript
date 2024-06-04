0f6d2e8f31b2a0ed5321beeee164cbee82cec8d2
dy <gkekdus97@naver.com>
2024-06-04 22:16:29 +0900

    feat: 버튼 및 추가 삭제 로직

--- js/Todo.js
+++ js/Todo.js
@@ -1,4 +1,19 @@
 // 추가 버튼
 document.createElementById("add").addEventListener("click", () => {
   let value = document.getElementById("content").value;
+  if (value) addTodo(value);
 });
+
+const addTodo = (text) => {
+  let item = document.createElement("li");
+  item.innerText = text;
+
+  let buttons = document.createElement("div");
+  buttons.classList.add("buttons");
+
+  let add = document.createElement("div");
+  add.classList.add("add");
+
+  let remove = document.createElement("div");
+  remove.classList.add("remove");
+};
