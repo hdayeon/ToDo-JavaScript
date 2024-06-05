47cc41968bd8da36b859695588f03aa19d21c0d5
dy <gkekdus97@naver.com>
2024-06-05 11:46:23 +0900

    feat: js 로직 작성중

--- js/Todo.js
+++ js/Todo.js
@@ -1,19 +1,31 @@
 // 추가 버튼
 document.createElementById("add").addEventListener("click", () => {
   let value = document.getElementById("content").value;
-  if (value) addTodo(value);
+  if (value) {
+    addTodo(value);
+    value = "";
+  }
 });
 
 const addTodo = (text) => {
+  let list = document.getElementById("todo");
+
   let item = document.createElement("li");
   item.innerText = text;
 
   let buttons = document.createElement("div");
   buttons.classList.add("buttons");
 
-  let add = document.createElement("div");
-  add.classList.add("add");
+  // let complete = document.createElement("button");
+  // add.classList.add("complete");
 
-  let remove = document.createElement("div");
+  let remove = document.createElement("button");
   remove.classList.add("remove");
+  remove.innerText = "삭제";
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+
+  list.insertBefore(item, list.childNodes[0]);
 };
